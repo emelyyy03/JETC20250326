defc965b2bd2fa55a15686af456b3f2727b922c5
Johana Emely Tobar Cruz <emelyyy03@outlook.com>
2025-03-26 07:45:13 -0600

    Agregar archivos de proyecto.

---
+++ JETC20250326.AppWebVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JETC20250326.AppWebVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
